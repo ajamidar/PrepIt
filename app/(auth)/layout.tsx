09d518782b10361d793148a91ba701df12e59793
ajamidar <jamidararnav@gmail.com>
2025-07-04 15:02:02 +0100

    Redirects put in place
    Now a user-authentication check occurs using cookies to check whether user can be directed straight to home page or if they have to get there after /sign-in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,12 @@
+import { isAuthenticated } from '@/lib/actions/auth.action';
+import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react'
 
-const Authlayout = ({ children }: { children: ReactNode}) => {
+const Authlayout = async ({ children }: { children: ReactNode}) => {
+  const isUserAuthenicated = await isAuthenticated();
+
+  if(isUserAuthenicated) redirect('/');
+
   return (
     <div className='auth-layout'>{children}</div>
   )
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
+import { isAuthenticated } from '@/lib/actions/auth.action'
 import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React, { ReactNode } from 'react'
 
-const RootLayout = ({ children }: { children: ReactNode}) => {
+const RootLayout = async ({ children }: { children: ReactNode}) => {
+  const isUserAuthenicated = await isAuthenticated();
+
+  if(!isUserAuthenicated) redirect('/sign-in');
+
   return (
     <div className='root-layout'>
       <nav>
