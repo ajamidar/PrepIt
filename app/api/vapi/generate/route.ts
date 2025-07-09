6ad9ffa5091bed65fa75efc337d29b7fd710c3be
ajamidar <jamidararnav@gmail.com>
2025-07-09 21:17:44 +0100

    Update route.ts
    Change in AI prompt;
    In attempt to make questions harder and similar to LeetCode.

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -20,6 +20,7 @@ export async function POST(request: Request) {
                     The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
                     Return the questions formatted like this:
                     ["Question 1", "Question 2", "Question 3"]
+                    Try to make the difficulty level similar to that of LeetCode's questions.
                     
                     Thank you! <3`,
         });
