8a7e3a94f3e10627fd23e890b0858d4683f17155
ajamidar <jamidararnav@gmail.com>
2025-07-20 22:42:57 +0100

    Update general.action.ts
    Now the most recently created feedback is displayed on the feedback page

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -99,12 +99,14 @@ export async function getFeedbackByInterviewId(
     .collection("feedback")
     .where("interviewId", "==", interviewId)
     .where("userId", "==", userId)
-    .limit(1)
+    .where("createdAt", ">=", "")
+    .orderBy("createdAt", "desc")
     .get();
 
   if (querySnapshot.empty) return null;
 
   const feedbackDoc = querySnapshot.docs[0];
+  
   return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
 }
 
