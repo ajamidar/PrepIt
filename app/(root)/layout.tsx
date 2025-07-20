36d09bf2c9f6ac0f85c08bd49a71670d02382182
ajamidar <jamidararnav@gmail.com>
2025-07-20 22:00:26 +0100

    NavBar made as a seperate component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,22 +15,7 @@ const RootLayout = async ({ children }: { children: ReactNode}) => {
 
   return (
     <div className='root-layout'>
-      <nav className='py-4 max-sm:px-8 flex flex-row justify-between items-center'>
-        <Link href="/" className='flex items-center justify-start gap-1 link pl-1'>
-              <div className='flex flex-col justify-start'>
-                  <div className='flex flex-row gap-1'>
-                  <Image src="/logo4.svg" alt="logo" width={50} height={38} />
-                  <h1 className='prep-it'>PrepIt</h1>
-                </div>
-                <div className='flex flex-row'>
-                  <p className='under-root-logo'>World's</p>
-                  <p className='under-root-logo text-[#ffb700]! font-bold'>&nbsp;#1</p>
-                  <p className='under-root-logo'>&nbsp;AI-Powered Interview preparation tool</p>
-                </div>
-              </div>
-            </Link>
-            <NavBar />
-      </nav>
+      <NavBar />
       <hr className='mb-3' />
 
       {children}
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -22,7 +22,21 @@ const NavBar = () => {
 
     return (
         <>
-        {homeStatus ==  'HOME' ? 
+        <nav className='py-4 max-sm:px-8 flex flex-row justify-between items-center'>
+        <Link href="/" className='flex items-center justify-start gap-1 link pl-1' onClick={handleHomeRedirect}>
+              <div className='flex flex-col justify-start'>
+                  <div className='flex flex-row gap-1'>
+                  <Image src="/logo4.svg" alt="logo" width={50} height={38} />
+                  <h1 className='prep-it'>PrepIt</h1>
+                </div>
+                <div className='flex flex-row'>
+                  <p className='under-root-logo'>World's</p>
+                  <p className='under-root-logo text-[#ffb700]! font-bold'>&nbsp;#1</p>
+                  <p className='under-root-logo'>&nbsp;AI-Powered Interview preparation tool</p>
+                </div>
+              </div>
+            </Link>
+            {homeStatus ==  'HOME' ? 
         <Link href="/account" className='flex items-center link pr-1' onClick={handleAccountRedirect}>
           <div className='flex flex-col w-full items-center gap-0.5'>
             <Image src='/image1.jpeg' alt="user-account" width={50} height={50} className='rounded-full' />
@@ -36,6 +50,8 @@ const NavBar = () => {
             <p className='text-[12px] text-black font-medium text-center'>Home Page</p>
           </div>
         </Link>}
+      </nav>
+        
         </>
     )
 }
