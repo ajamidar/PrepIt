cdd44d014ea526b19563fa97a18ca739b169dd88
ajamidar <jamidararnav@gmail.com>
2025-07-23 22:36:02 +0100

    update name functionality completed

--- app/components/Account.tsx
+++ app/components/Account.tsx
@@ -4,6 +4,12 @@ import { getCurrentUser } from '@/lib/actions/auth.action'
 import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
 import { updateUserName } from '@/lib/actions/extra.action';
+import { toast } from 'sonner';
+
+enum EditOrNoEdit{
+    EDIT = 'EDIT',
+   NO = 'NO',
+}
 
 const Account = () => {
     const [user, setUser] = useState<any>(null);
@@ -26,10 +32,21 @@ const Account = () => {
         setIsUpdating(true);
         await updateUserName(user?.id, newName); // 👈 make sure to pass UID or user id
         setUser({ ...user, name: newName });
-        setIsUpdating(false);
+        setIsUpdating(false)
+        toast.success("Your name has been updated!")
+        setEditStatus(EditOrNoEdit.NO);
     };
 
-    let editName = false;
+
+    const [editStatus, setEditStatus] = useState<EditOrNoEdit>(EditOrNoEdit.NO);
+    
+    const handleEditRedirect = async() => {
+        setEditStatus(EditOrNoEdit.EDIT);
+    }
+    
+    const handleNoEditRedirect = async() => {
+        setEditStatus(EditOrNoEdit.NO);
+    }
 
     return (
         <div className='flex flex-col justify-center items-center gap-4 dark-gradient mt-4 mb-4 py-16 rounded-4xl w-1/2 self-center-safe'>
@@ -45,8 +62,20 @@ const Account = () => {
                 <p className='text-black text-[20px]'><strong>Email:</strong> {user?.email}</p>
             </div>
 
-            {editName ?
-            "..."
+            {editStatus == "NO" ?
+            <div className='flex flex-col items-center gap-0.5'>
+                <div className='flex flex-row items-center gap-2'>
+                    <label htmlFor='name' className='text-black text-[20px] font-bold'>Name:</label>
+                    <p className='text-black text-[20px]'> {user?.name}</p>
+                </div>
+                <button
+                    onClick={handleEditRedirect}
+                    disabled={isUpdating}
+                    className='bg-blue-500 text-white px-2 py-1 rounded text-sm disabled:opacity-50 text-[15px] hover:bg-[#2744ff]! cursor-pointer'
+                >
+                    Edit Name
+                </button>
+            </div>
             : 
             <div className='flex flex-col items-center gap-2'>
                 <label htmlFor='name' className='text-black text-[20px] font-bold'>Edit Name:</label>
@@ -60,9 +89,10 @@ const Account = () => {
                 <button
                     onClick={handleUpdateName}
                     disabled={isUpdating}
-                    className='bg-blue-500 text-white px-3 py-1 rounded text-sm disabled:opacity-50 text-[20px]'
+                    className='bg-blue-500 text-white px-3 py-1 rounded text-sm disabled:opacity-50 text-[17px]'
                 >
-                    {isUpdating ? 'Updating...' : 'Update Name'}
+                    {isUpdating ?  'Updating...' 
+                     : 'Update Name'}
                 </button>
             </div>}
             
